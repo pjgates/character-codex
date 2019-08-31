bee71f425772a87863ad6d189a3f419f165cc9f9
Peter Gates <peter@Peters-MBP.gateway>
2019-08-31 15:54:15 +1000

    Updates

---
+++ precache-manifest.4413929f3f2d9cdd65d5dba8ccbd01f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d253b59f79429729bf645424065c4c2a",
+    "url": "/character-codex/index.html"
+  },
+  {
+    "revision": "a3199df3b8c9c8e56320",
+    "url": "/character-codex/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "194f0bbec31629feeabb",
+    "url": "/character-codex/static/js/2.f7915d26.chunk.js"
+  },
+  {
+    "revision": "a3199df3b8c9c8e56320",
+    "url": "/character-codex/static/js/main.be16b913.chunk.js"
+  },
+  {
+    "revision": "d2dd2d047066b8168f87",
+    "url": "/character-codex/static/js/runtime~main.2b904896.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/character-codex/static/media/logo.5d5d9eef.svg"
+  }
+]);
